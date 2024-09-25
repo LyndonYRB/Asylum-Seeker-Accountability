0af23e021036d44032d15d64e0f8ba7403ab723a
Lyndon <lyndon.stluce@gmail.com>
2024-09-25 18:02:56 -0400

    Add start script for backend

--- server/index.js
+++ server/index.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 5000;
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-// API endpoint for testing
-app.get('/api', (req, res) => {
-  res.json({ message: "Hello from the backend!" });
-});
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+  res.send('Server is running!');
 });
 
 app.listen(PORT, () => {
