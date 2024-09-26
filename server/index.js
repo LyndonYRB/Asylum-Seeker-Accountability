95a8f6b01105e2e19a02a1d3344e07abbb78d47a
Lyndon <lyndon.stluce@gmail.com>
2024-09-26 17:28:31 -0400

    Set up MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const connectDB = require('./db'); // Import the database connection file
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
-});
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.json());
+
+// Your routes will go here
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
